d057a633c98cf0dede032cf9a8b3855908696bd8
Leandro-Aguiar <leosaguiarl@yahoo.com.br>
2022-07-23 19:26:11 -0300

    Checagem da idade ok

--- Sistema de Cadastro de Eventos/index.js
+++ Sistema de Cadastro de Eventos/index.js
@@ -2,9 +2,19 @@
 
 const de = 30;
 let da = 26;
+let idade = 20;
 
 if(da < de){
-    console.log("Permitir Evento")
+    console.log("Evento Permitido")
+
+    if(idade > 18) {
+        console.log("Cadastro Permitido");
+
+    }
+    else {
+        console.log("Idade não Permitida");
+    }
+
 } else {
     console.log("Data invalida")
 }
