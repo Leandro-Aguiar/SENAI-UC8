4df965abf3b43fdd9c37609608c5ec1dea720069
Leandro-Aguiar <leosaguiarl@yahoo.com.br>
2022-07-23 19:40:11 -0300

    Colocando significado das variaveis

--- Sistema de Cadastro de Eventos/index.js
+++ Sistema de Cadastro de Eventos/index.js
@@ -1,14 +1,25 @@
 // Algoritmo Sistema de Cadastro
+//de --> Data do Evento, da --> Data Atual, PA --> Quantidade de Participantes
 
 const de = 30;
 let da = 26;
 let idade = 20;
+let PA = 80;
 
 if(da < de){
     console.log("Evento Permitido")
 
-    if(idade > 18) {
+    if(idade > 18) 
+    {
         console.log("Cadastro Permitido");
+
+        if(PA < 100){
+            console.log("Cadastro Permitido por Quantidade de Participantes")
+        } 
+        else
+        {
+            console.log("Quantidade de Participantes Excedida")
+        }
 
     }
     else {
